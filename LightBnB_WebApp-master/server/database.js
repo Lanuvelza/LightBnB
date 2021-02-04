67762d221614cd47ac33e4b3104da1d92380db6b
Lanuvelza <ianlam1221@hotmail.com>
2021-02-04 18:10:11 +0000

    implemented connection to lightbnb database in psql and refactored getAllproperties function to use postgres query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,18 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Client } = require('pg'); 
+
+const client = new Client({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+client.connect()
+.then(() => {
+  console.log('Connected');
+});
 
 /// Users
 
@@ -67,11 +80,18 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+
+  return client.query(`
+  SELECT * FROM properties
+  LIMIT $1;
+  `, [limit])
+  .then(res => res.rows);
+
+  // const limitedProperties = {};
+  // for (let i = 1; i <= limit; i++) {
+  //   limitedProperties[i] = properties[i];
+  // }
+  // return Promise.resolve(limitedProperties);
 }
 exports.getAllProperties = getAllProperties;
 
