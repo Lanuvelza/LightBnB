fee27ff4974b0cf779aa08d175f9c0e11f77a7eb
Lanuvelza <ianlam1221@hotmail.com>
2021-02-04 21:42:02 +0000

    update addProperty function to use lightbnb database with SQL queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -55,8 +55,10 @@ exports.getUserWithId = getUserWithId;
 const addUser =  function(user) {
   return pool.query(`
   INSERT INTO users (name, email, password) 
-  VALUES ($1, $2, $3);
-  `, [user.name, user.email, user.password]); 
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `, [user.name, user.email, user.password])
+  .then(res => res.rows); 
 }
 exports.addUser = addUser;
 
@@ -97,7 +99,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `; 
 
   //3 
@@ -164,9 +166,43 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(`
+  INSERT INTO properties (
+    owner_id, 
+    title, 
+    description, 
+    thumbnail_photo_url, 
+    cover_photo_url,
+    cost_per_night,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms,
+    country,
+    street, 
+    city,
+    province,
+    post_code
+    ) 
+    VALUES 
+    ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  
+    RETURNING *;
+  `, [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    (property.cost_per_night * 100),
+    property.parking_spaces, 
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    property.country,
+    property.street, 
+    property.city,
+    property.province,
+    property.post_code
+  ])
+  .then(res => res.rows);
 }
 exports.addProperty = addProperty;
